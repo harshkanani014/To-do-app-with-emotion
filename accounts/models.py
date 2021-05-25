ee13a781379ac987784b3df778179596cae1b41c
harshkanani014 <harshkanani100@gmail.com>
2021-05-26 01:28:03 +0530

    user model change

--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    gender = models.TextField(max_length=50)
-    age = models.IntegerField()
-    email = models.EmailField(unique=True)
+    username = models.TextField(max_length=50, default="a")
+    gender = models.TextField(max_length=50, default="None")
+    age = models.IntegerField(default="0")
+    email = models.EmailField(unique=True, default="harsh@gmail.com")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
     USERNAME_FIELD = 'email'
     
 
